082657eeae839268cb31b605dfcfb08ad1974e12
Yesenia <yeseniasescarcega@gmail.com>
2017-07-18 13:44:32 -0500

    Se agregan imágenes del carrusel y botón de registrarse.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,41 @@
 console.log("JS conectado");
+
+// ****** Código API ******
+// Petición al servidor
+function getJSON(url) {
+	return new Promise(function (resolve, reject) {
+		var ajax = new XMLHttpRequest();
+
+		ajax.open("GET", url),
+		ajax.send();
+		ajax.onreadystatechange = function () {
+			if (ajax.readyState == 4) {
+				// resolve(JSON.parse(ajax.responseText));
+			}
+		}
+	});
+}
+
+getJSON("api/users.js")
+.then(function (datos) {
+	console.log(datos);
+	return getJSON(dato);
+})
+.then(function (datosReturn) {
+	datosReturn.forEach(function (algo) {
+		objectToDom(algo);
+	})
+});
+
+// function objectToDom(algo) {
+// 	var div = document.createElement('div');
+// 	div.innerText
+// }
+// **** FUNCIONES ***
+
+function cargarFunciones() {
+	 $('.carousel.carousel-slider').carousel({fullWidth: true});
+}
+
+
+$(document).ready( cargarFunciones);
