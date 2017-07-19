aa383a05dbc0bddacca30796bfef6789fd465506
Yesenia <yeseniasescarcega@gmail.com>
2017-07-19 15:06:34 -0500

    Se agregan funciones al main.js para la validación de datos y consumo de la api.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,67 @@
-console.log("JS conectado");
+// console.log("JS conectado");
+// **** VARIABLES ****
+var $input = $('#inputTel');
+var $check = $('#checkTerminos');
 
-// ****** Código API ******
-// Petición al servidor
-function getJSON(url) {
-	return new Promise(function (resolve, reject) {
-		var ajax = new XMLHttpRequest();
-
-		ajax.open("GET", url),
-		ajax.send();
-		ajax.onreadystatechange = function () {
-		}
-	});
+// **** FUNCIONES ****
+function verificarLongNum($input) {
+	// console.log($input);
+	return $input.val().length == 10;
 }
 
-getJSON("http://localhost:3000/api/registerNumber");
-
-// **** FUNCIONES ***
-function verificarLongNum() {
-	var $input = $('#inputTel').val().length;
-	console.log($input);
-	return $input == 10;
-}
-
-function verificarCheckbox() {
-	var $check = $('#checkTerminos');
-	console.log($check.is(':checked'));
-	return $check.is(':checked');
+function verificarCheckbox($check) {
+	var habilitado = $check.is(':checked');
+	console.log(habilitado);
+	return habilitado;
 }
 
 function habilitarBtn() {
 	var $btn = $('#btnContinuar');
-
-	if(verificarLongNum() && verificarCheckbox()){
+	if(verificarLongNum($input) && verificarCheckbox($check)){
 		$btn.removeClass('disabled');
+		$btn.on('click',cargarPeticiones);
 	}
 	else{
 		$btn.addClass('disabled');
 	}
 }
 
-function validacion() {
+function validacionItems() {
 	$('#inputTel').on('change', habilitarBtn);
 	$('#checkTerminos').change(habilitarBtn);
 }
 
 function cargarFunciones() {
 	 $('.carousel.carousel-slider').carousel({fullWidth: true});
-	 $('select').material_select();
-	 validacion();
+	 $('select').material_select();//dropdown
+	 validacionItems();
+}
+
+// ****** API ******
+
+var url = {
+	regNum : "http://localhost:3000/api/registerNumber",
+	resendCode : "http://localhost:3000/api/resendCode",
+	createUser : "http://localhost:3000/api/createUser",
+	regCard : "http://localhost:3000/api/registerCard"
+};
+
+function cargarPeticiones() {
+	$.post(url.regNum,{
+		"phone": $input.val(),
+		"terms": verificarCheckbox($check)
+	}).then(function (response) {
+		console.log(response);
+	}).fail(function (error) {
+		console.log(error);
+	});
+
+
+	$.post(url.resendCode,{
+		"phone": $input.val()
+	}).then(function (response) {
+		console.log(response);
+	});
 }
 
 
