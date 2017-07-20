a082cd5ba493fb27e706ea197b530a39921a4c2b
Inti15 <yeseniasescarcega@gmail.com>
2017-07-20 03:34:30 -0500

    Se agregaron funciones para localStorage, el temporizador para validar el codigo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,16 +2,43 @@
 // **** VARIABLES ****
 var $input = $('#inputTel');
 var $check = $('#checkTerminos');
+var $txtNum = $('#textoNumero');
+var codigoGenerado = 0;
 
 // **** FUNCIONES ****
+
+function almacenar() {
+	console.log($input.val());
+	console.log(codigoGenerado);
+	localStorage.setItem("id", $input.val());
+	localStorage.setItem("code", codigoGenerado);
+}
+
+function leerDatos() {
+	console.log(localStorage.getItem("id"));
+	$txtNum.append("<b>" + localStorage.getItem("id") + "</b>");
+	var segundos = 20;
+	var temporizador = setInterval(function(){
+		if(segundos == 0){
+			clearInterval(temporizador);
+		}
+		$("#temporizador").text(segundos--);
+	}, 1000);
+}
+
 function verificarLongNum($input) {
 	// console.log($input);
-	return $input.val().length == 10;
+	if(/^([0-9])*$/.test($input.val()) && $input.val().length == 10){
+		return true;
+	}
+	else{
+		alert("Número no valido");
+		return false;
+	}
 }
 
 function verificarCheckbox($check) {
 	var habilitado = $check.is(':checked');
-	console.log(habilitado);
 	return habilitado;
 }
 
@@ -19,7 +46,13 @@ function habilitarBtn() {
 	var $btn = $('#btnContinuar');
 	if(verificarLongNum($input) && verificarCheckbox($check)){
 		$btn.removeClass('disabled');
-		$btn.on('click',cargarPeticiones);
+		generarCodigo();
+		validarCodigo();
+		$btn.click(function () {
+			almacenar();
+			$input.val("");
+			$check.removeAttr("checked");
+		});
 	}
 	else{
 		$btn.addClass('disabled');
@@ -46,23 +79,26 @@ var url = {
 	regCard : "http://localhost:3000/api/registerCard"
 };
 
-function cargarPeticiones() {
+function generarCodigo() {
 	$.post(url.regNum,{
 		"phone": $input.val(),
-		"terms": verificarCheckbox($check)
+		"terms": 'true'
 	}).then(function (response) {
 		console.log(response);
 	}).fail(function (error) {
 		console.log(error);
 	});
 
+}
 
+function validarCodigo(){
 	$.post(url.resendCode,{
 		"phone": $input.val()
 	}).then(function (response) {
 		console.log(response);
+		console.log(response.data);
+		codigoGenerado = response.data;
 	});
 }
-
 
 $(document).ready( cargarFunciones);
